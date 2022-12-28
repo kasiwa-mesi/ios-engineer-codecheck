4b77a03c8b2907f84834fbc53376277350960639
kasiwa-mesi <italynataly@outlook.jp>
2022-12-28 17:24:33 +0900

    :adhesive_bandage: RepositoryModelを修正

--- iOSEngineerCodeCheck/Model/RepositoryModel.swift
+++ iOSEngineerCodeCheck/Model/RepositoryModel.swift
@@ -16,7 +16,6 @@ struct RepositoryModel: Codable {
     let id: Int
     let name: String
     let fullName: String
-    var urlStr: String { "https://github.com/\(fullName)" }
     let language: String?
     let starsCount: Int
     let watchersCount: Int
